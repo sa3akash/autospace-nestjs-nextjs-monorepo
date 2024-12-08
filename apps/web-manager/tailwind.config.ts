11155ffa6172b0091f95660d9c0bd6633f4e2c15
sa3akash <sa3akash@gmail.com>
2024-12-09 03:29:10 +0600

    setup admin and manager

---
+++ apps/web-manager/tailwind.config.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+import type { Config } from 'tailwindcss';
+
+export default {
+  presets: [require('../../libs/ui/tailwind.config')],
+  content: ['./src/**/*.{ts,tsx}', '../../libs/ui/**/*.{ts,tsx}'],
+} satisfies Config;
